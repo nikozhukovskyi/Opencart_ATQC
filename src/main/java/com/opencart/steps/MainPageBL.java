75bb69cd85788fb51bdcabbcd5aaa7c57b285f01
Mykola Zhukovskyi <zhukovskyi.nikolay@gmail.com>
2020-11-15 15:04:49 +0200

    Success Login

--- src/main/java/com/opencart/steps/MainPageBL.java
+++
@@ -1,9 +0,0 @@
-package com.opencart.steps;
-
-import com.opencart.pages.HeaderPage;
-
-public class MainPageBL {
-    public HeaderPageBL getHeaderPageBL() {
-        return new HeaderPageBL();
-    }
-}
